519962297a89814d0e20e27a4c1bd31be7ec7006
Lionel Jouin <lionel.jouin@est.tech>
2021-02-25 15:05:25 +0100

    Proxy error handling

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -23,8 +23,14 @@ func (p *Proxy) InterfaceCreated(intf *networking.Interface) {
 	p.bridge.LinkInterface(intf)
 	// Add addresses to the bridge and remove them from the interface
 	for _, ip := range intf.LocalIPs {
-		p.bridge.AddAddress(ip)
-		intf.RemoveAddress(ip)
+		err := p.bridge.AddAddress(ip)
+		if err != nil {
+			logrus.Errorf("Proxy: Error adding IP on the bridge: %v", err)
+		}
+		err = intf.RemoveAddress(ip)
+		if err != nil {
+			logrus.Errorf("Proxy: Error remove IP from an interface: %v", err)
+		}
 	}
 	if intf.InteraceType == networking.NSC {
 		// Add the neighbor IPs of the interface to the nexthops (outgoing traffic)
@@ -43,8 +49,15 @@ func (p *Proxy) InterfaceDeleted(intf *networking.Interface) {
 	p.bridge.UnLinkInterface(intf)
 	// Add addresses to the interface and remove them from the bridge
 	for _, ip := range intf.LocalIPs {
-		p.bridge.RemoveAddress(ip)
-		intf.AddAddress(ip)
+		err := p.bridge.RemoveAddress(ip)
+		if err != nil {
+			logrus.Errorf("Proxy: Error remove IP from the bridge: %v", err)
+		}
+		// TODO
+		err = intf.AddAddress(ip)
+		if err != nil {
+			logrus.Errorf("Proxy: Error adding IP on an interface: %v", err)
+		}
 	}
 	if intf.InteraceType == networking.NSC {
 		// Remove the neighbor IPs of the interface from the nexthops (outgoing traffic)
