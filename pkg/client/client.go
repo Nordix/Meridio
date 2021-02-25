94d9d9fc7e5165b67d784e08c77e3834e179e171
Lionel Jouin <lionel.jouin@est.tech>
2021-02-25 15:51:38 +0100

    NSC set mechanism preferences with interface name

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,10 +3,15 @@ package client
 import (
 	"fmt"
 	"math/rand"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/cls"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -17,6 +22,7 @@ type NetworkServiceClient struct {
 	Labels                     map[string]string
 	Connection                 *networkservice.Connection
 	nsmgrClient                NSMgrClient
+	InterfaceName              string
 }
 
 type NSMgrClient interface {
@@ -31,10 +37,11 @@ func (nsc *NetworkServiceClient) Request() {
 		var err error
 		nsc.Connection, err = nsc.nsmgrClient.Request(request)
 		if err != nil {
-			time.Sleep(2 * time.Second)
-			logrus.Errorf("Full Mesh Client: NetworkServiceClient Request err: %v", err)
+			time.Sleep(15 * time.Second)
+			logrus.Errorf("Network Service Client: Request err: %v", err)
 			continue
 		}
+		// TODO interface adverstise
 		break
 	}
 }
@@ -44,18 +51,30 @@ func (nsc *NetworkServiceClient) Close() {
 	var err error
 	_, err = nsc.nsmgrClient.Close(nsc.Connection)
 	if err != nil {
-		time.Sleep(2 * time.Second)
-		logrus.Errorf("Full Mesh Client: NetworkServiceClient Close err: %v", err)
+		logrus.Errorf("Network Service Client: Close err: %v", err)
 	}
 }
 
 func (nsc *NetworkServiceClient) prepareRequest() *networkservice.NetworkServiceRequest {
+	// TODO
+	outgoingMechanism := &networkservice.Mechanism{
+		Cls:  cls.LOCAL,
+		Type: kernel.MECHANISM,
+		Parameters: map[string]string{
+			kernel.NetNSURL:         (&url.URL{Scheme: "file", Path: "/proc/thread-self/ns/net"}).String(),
+			kernel.InterfaceNameKey: nsc.InterfaceName,
+		},
+	}
+
 	request := &networkservice.NetworkServiceRequest{
 		Connection: &networkservice.Connection{
 			Id:                         nsc.Id,
 			NetworkService:             nsc.NetworkServiceName,
 			Labels:                     nsc.Labels,
 			NetworkServiceEndpointName: nsc.NetworkServiceEndpointName,
+		},
+		MechanismPreferences: []*networkservice.Mechanism{
+			outgoingMechanism,
 		},
 	}
 	return request
@@ -66,10 +85,15 @@ func NewNetworkServiceClient(networkServiceName string, nsmgrClient NSMgrClient)
 	identifier := rand.Intn(100)
 	id := fmt.Sprintf("%d", identifier)
 
+	// TODO
+	randomID := rand.Intn(1000)
+	interfaceName := "nsc" + strconv.Itoa(randomID)
+
 	networkServiceClient := &NetworkServiceClient{
 		Id:                 id,
 		NetworkServiceName: networkServiceName,
 		nsmgrClient:        nsmgrClient,
+		InterfaceName:      interfaceName,
 	}
 
 	return networkServiceClient
