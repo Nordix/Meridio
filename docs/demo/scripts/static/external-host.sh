bbe6a52a7827be709b65e472f607829ad829cfe6
Lionel Jouin <lionel.jouin@est.tech>
2021-04-15 15:14:09 +0200

    updated demo instructions + vlan instructions

--- docs/demo/scripts/static/external-host.sh
+++ docs/demo/scripts/static/external-host.sh
@@ -13,8 +13,12 @@ docker exec -it ubuntu-ext gunzip ctraffic.gz
 docker exec -it ubuntu-ext chmod u+x ctraffic
 docker exec -it ubuntu-ext mv ctraffic /usr/bin/
 
+docker exec -it ubuntu-ext sysctl -w net.ipv6.conf.all.disable_ipv6=0
 docker exec -it ubuntu-ext sysctl -w net.ipv4.fib_multipath_hash_policy=1
 docker exec -it ubuntu-ext sysctl -w net.ipv6.fib_multipath_hash_policy=1
+docker exec -it ubuntu-ext sysctl -w net.ipv6.conf.all.forwarding=1
+docker exec -it ubuntu-ext sysctl -w net.ipv4.conf.all.forwarding=1
+docker exec -it ubuntu-ext sysctl -w net.ipv6.conf.all.accept_dad=0
 
 vlan_id="1500"
 parent_if_name="eth0"
@@ -26,10 +30,15 @@ do
     vni=$((vlan_id + id))
     if_name="${vlan_if_name}-${id}"
     ip="192.168.${id}.150/24"
+    ip6="1500:${id}::150/64"
 
     docker exec -it ubuntu-ext ip link add $if_name type vxlan id $vni group 239.1.$id.1 dev $parent_if_name dstport 4789
     docker exec -it ubuntu-ext ip addr add $ip dev $if_name
+    docker exec -it ubuntu-ext ip addr add $ip6 dev $if_name
     docker exec -it ubuntu-ext ip link set $if_name up
 
     echo "if name: $if_name ; vni: $vni ; ip: $ip"
 done
+
+docker exec -it ubuntu-ext ip route replace 20.0.0.1/32 nexthop via 192.168.1.1 nexthop via 192.168.2.1
+docker exec -it ubuntu-ext ip route replace 2000::1/128 nexthop via 1500:1::1 nexthop via 1500:2::1
