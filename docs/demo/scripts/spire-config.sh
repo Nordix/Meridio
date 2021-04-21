8c1b95a83c0c18ee56d0f4fd388f3e5efb08b231
Lionel Jouin <lionel.jouin@est.tech>
2021-04-21 13:36:43 +0200

    Namespaces (trenches) support on static script for external links

--- docs/demo/scripts/spire-config.sh
+++ docs/demo/scripts/spire-config.sh
@@ -9,10 +9,17 @@ kubectl -n spire exec spire-server-0 -- \
 -selector k8s_sat:agent_sa:spire-agent \
 -node
 
+kubectl exec -n spire spire-server-0 -- \
+/opt/spire/bin/spire-server entry create \
+-spiffeID spiffe://example.org/ns/nsm-system/sa/default \
+-parentID spiffe://example.org/ns/spire/sa/spire-agent \
+-selector k8s:ns:nsm-system \
+-selector k8s:sa:default
+
 kubectl -n spire exec spire-server-0 -- \
 /opt/spire/bin/spire-server entry create \
 -ttl 72000 \
--spiffeID spiffe://example.org/ns/nsm-system/sa/default \
+-spiffeID spiffe://example.org/ns/default/sa/default \
 -parentID spiffe://example.org/ns/spire/sa/spire-agent \
 -selector k8s:ns:default \
 -selector k8s:sa:default
