0ed84354351c3fef07582e9df19a1b3682df6a56
Lionel Jouin <lionel.jouin@est.tech>
2021-09-10 10:53:55 +0200

    Fix documentation and unused spire entries removed

--- docs/demo/scripts/spire-config.sh
+++ docs/demo/scripts/spire-config.sh
@@ -9,13 +9,6 @@ kubectl -n spire exec spire-server-0 -- \
 -selector k8s_sat:agent_sa:spire-agent \
 -node
 
-kubectl exec -n spire spire-server-0 -- \
-/opt/spire/bin/spire-server entry create \
--spiffeID spiffe://example.org/ns/nsm-system/sa/default \
--parentID spiffe://example.org/ns/spire/sa/spire-agent \
--selector k8s:ns:nsm-system \
--selector k8s:sa:default
-
 kubectl -n spire exec spire-server-0 -- \
 /opt/spire/bin/spire-server entry create \
 -ttl 72000 \
@@ -23,19 +16,3 @@ kubectl -n spire exec spire-server-0 -- \
 -parentID spiffe://example.org/ns/spire/sa/spire-agent \
 -selector k8s:ns:default \
 -selector k8s:sa:default
-
-kubectl exec -n spire spire-server-0 -- \
-/opt/spire/bin/spire-server entry create \
--spiffeID spiffe://example.org/ns/nsm-system/sa/default \
--parentID spiffe://example.org/ns/spire/sa/spire-agent \
--selector k8s:ns:nsm-system \
--selector k8s:sa:meridio
-
-kubectl -n spire exec spire-server-0 -- \
-/opt/spire/bin/spire-server entry create \
--ttl 72000 \
--spiffeID spiffe://example.org/ns/default/sa/default \
--parentID spiffe://example.org/ns/spire/sa/spire-agent \
--selector k8s:ns:default \
--selector k8s:sa:meridio
-
