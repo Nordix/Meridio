b6e177743708e2c310ad00dd13cb6f4d5df272ae
Lionel <lionel.jouin@est.tech>
2022-01-24 15:14:19 +0100

    Fix Target ambassador disconnect
    A wrong context was used to create the Network service client
    The target could not disconnect from the proxy (context cancelled)

--- pkg/target/conduit/conduit.go
+++ pkg/target/conduit/conduit.go
@@ -102,7 +102,8 @@ func (c *Conduit) Connect(ctx context.Context) error {
 		RequestTimeout: c.nsmConfig.RequestTimeout,
 		ConnectTo:      c.nsmConfig.ConnectTo,
 	}
-	c.networkServiceClient = client.NewSimpleNetworkServiceClient(ctx, clientConfig, c.apiClient, c.getAdditionalFunctionalities(ctx))
+	nscCtx := context.Background()
+	c.networkServiceClient = client.NewSimpleNetworkServiceClient(nscCtx, clientConfig, c.apiClient, c.getAdditionalFunctionalities(nscCtx))
 	err := c.networkServiceClient.Request(&networkservice.NetworkServiceRequest{
 		Connection: &networkservice.Connection{
 			Id:             fmt.Sprintf("%s-%s-%d", c.nsmConfig.Name, proxyNetworkServiceName, 0),
