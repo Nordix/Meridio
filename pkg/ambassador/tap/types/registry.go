3152638aa8147e1ff59c94620d168100edd513b2
Lionel Jouin <lionel.jouin@est.tech>
2022-05-25 18:35:34 +0200

    Replace make mocks by make generate
    go:generate directive added to each file for the mock files

--- pkg/ambassador/tap/types/registry.go
+++ pkg/ambassador/tap/types/registry.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:generate mockgen -source=registry.go -destination=mocks/registry.go -package=mocks
 package types
 
 import (
