a4f50a40584837f64715d8dd22335c0c8718281c
Lionel Jouin <lionel.jouin@est.tech>
2022-10-25 15:59:50 +0200

    CI: Kind environement fixes
    * Ignore gateway deletion errors
    * remove "-it" from gateway creation script for Kind
    * Fix test-scope.yaml Kubernetes versions

--- docs/demo/scripts/kind/external-host.sh
+++ docs/demo/scripts/kind/external-host.sh
@@ -11,19 +11,19 @@ do
     vi=$((vlan_id + index_trench * 100))
     container_name=${trenches[$index_trench]}
 
-    docker kill $container_name
-    docker rm $container_name
+    docker kill $container_name || true
+    docker rm $container_name || true
 
     docker run -t -d --network="kind" --name="$container_name" --privileged registry.nordix.org/cloud-native/meridio/kind-host:latest
     
-    docker exec -it $container_name sh -c "echo \"PS1='[GW/TG] $container_name | VLAN:${vi}..$(($vi + ${#vlans[@]}-1))> '\" >> ~/.bashrc"
+    docker exec $container_name sh -c "echo \"PS1='[GW/TG] $container_name | VLAN:${vi}..$(($vi + ${#vlans[@]}-1))> '\" >> ~/.bashrc"
 
-    docker exec -it $container_name sysctl -w net.ipv6.conf.all.disable_ipv6=0
-    docker exec -it $container_name sysctl -w net.ipv4.fib_multipath_hash_policy=1
-    docker exec -it $container_name sysctl -w net.ipv6.fib_multipath_hash_policy=1
-    docker exec -it $container_name sysctl -w net.ipv6.conf.all.forwarding=1
-    docker exec -it $container_name sysctl -w net.ipv4.conf.all.forwarding=1
-    docker exec -it $container_name sysctl -w net.ipv6.conf.all.accept_dad=0
+    docker exec $container_name sysctl -w net.ipv6.conf.all.disable_ipv6=0
+    docker exec $container_name sysctl -w net.ipv4.fib_multipath_hash_policy=1
+    docker exec $container_name sysctl -w net.ipv6.fib_multipath_hash_policy=1
+    docker exec $container_name sysctl -w net.ipv6.conf.all.forwarding=1
+    docker exec $container_name sysctl -w net.ipv4.conf.all.forwarding=1
+    docker exec $container_name sysctl -w net.ipv6.conf.all.accept_dad=0
 
     for (( index_vlan=0; index_vlan<=$((${#vlans[@]}-1)); index_vlan++ ))
     do
@@ -32,12 +32,12 @@ do
         ip="169.254.$((vlan_id + index_vlan)).150/24"
         ip6="100:$((vlan_id + index_vlan))::150/64"
         
-        docker exec -it $container_name ip link add link $parent_if_name name $if_name type vlan id $v_id
-        docker exec -it $container_name ip link set $if_name up
-        docker exec -it $container_name ip addr add $ip dev $if_name
-        docker exec -it $container_name ip addr add $ip6 dev $if_name
+        docker exec $container_name ip link add link $parent_if_name name $if_name type vlan id $v_id
+        docker exec $container_name ip link set $if_name up
+        docker exec $container_name ip addr add $ip dev $if_name
+        docker exec $container_name ip addr add $ip6 dev $if_name
 
-        docker exec -it $container_name ethtool -K $parent_if_name tx off
+        docker exec $container_name ethtool -K $parent_if_name tx off
     done
 
 done
