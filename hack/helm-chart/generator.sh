08c8d91a244061594122430b743c542434c2d508
Lionel Jouin <lionel.jouin@est.tech>
2023-01-31 19:02:33 +0100

    Helm chart generator: repository configurable
    Add possibility to set the registry, repository and NSM repository

--- hack/helm-chart/generator.sh
+++ hack/helm-chart/generator.sh
@@ -12,7 +12,14 @@ appVersion: '$VERSION'
 name: Meridio
 version: '$HELM_CHART_VERSION'""" > $MERIDIO_HELM_PATH/Chart.yaml
 
-    make -s print-manifests OPERATOR_NAMESPACE="{{.Release.Namespace}}" VERSION="$VERSION" REGISTRY="$REGISTRY" | yq '. | select(.kind != "Namespace" and .kind != "CustomResourceDefinition")' -s "\"$MERIDIO_HELM_PATH/templates/\" + .metadata.name + \"-\" + .kind"
+    echo """---
+registry: \"$REGISTRY\"
+repository: \"$REPOSITORY\"
+nsm:
+  repository: \"$NSM_REPOSITORY\"""" > $MERIDIO_HELM_PATH/values.yaml
+
+    make -s print-manifests OPERATOR_NAMESPACE="{{.Release.Namespace}}" VERSION="$VERSION" REGISTRY="{{.Values.registry}}/{{.Values.repository}}" NSM_REPOSITORY="{{.Values.nsm.repository}}" | yq '. | select(.kind != "Namespace" and .kind != "CustomResourceDefinition")' -s "\"$MERIDIO_HELM_PATH/templates/\" + .metadata.name + \"-\" + .kind"
+    sed -i '0,/  name: .*/s/^  name: .*/  name: meridio-operator-validating-webhook-configuration-{{.Release.Namespace}}/1' $MERIDIO_HELM_PATH/templates/meridio-operator-validating-webhook-configuration-ValidatingWebhookConfiguration.yml
     helm package $MERIDIO_HELM_PATH --version $HELM_CHART_VERSION --destination $HELM_CHART_PATH
 }
 
@@ -28,23 +35,23 @@ appVersion: '$VERSION'
 name: Meridio-CRDs
 version: '$HELM_CHART_VERSION'""" > $MERIDIO_CRDS_HELM_PATH/Chart.yaml
 
-    make -s print-manifests OPERATOR_NAMESPACE="{{.Release.Namespace}}" VERSION="$VERSION" REGISTRY="$REGISTRY" | yq '. | select(.kind == "CustomResourceDefinition")' -s "\"$MERIDIO_CRDS_HELM_PATH/templates/\" + .metadata.name"
+    make -s print-manifests OPERATOR_NAMESPACE="{{.Release.Namespace}}" VERSION="$VERSION" | yq '. | select(.kind == "CustomResourceDefinition")' -s "\"$MERIDIO_CRDS_HELM_PATH/templates/\" + .metadata.name"
     helm package $MERIDIO_CRDS_HELM_PATH --version $HELM_CHART_VERSION --destination $HELM_CHART_PATH
 }
 
 function chart_meridio_target () {
     cp -r $MERIDIO_TARGET_HELM_CURRENT_PATH $MERIDIO_TARGET_HELM_PATH
     cat $MERIDIO_TARGET_HELM_CURRENT_PATH/Chart.yaml | yq ".appVersion = \"$VERSION\" | .version = \"$HELM_CHART_VERSION\"" > $MERIDIO_TARGET_HELM_PATH/Chart.yaml
-    cat $MERIDIO_TARGET_HELM_CURRENT_PATH/values.yaml | yq ".tag = \"$VERSION\" | .tapa.version = \"$VERSION\" | .exampleTarget.version = \"$VERSION\"" > $MERIDIO_TARGET_HELM_PATH/values.yaml
+    cat $MERIDIO_TARGET_HELM_CURRENT_PATH/values.yaml | yq ".tag = \"$VERSION\" | .tapa.version = \"$VERSION\" | .exampleTarget.version = \"$VERSION\" | .registry = \"$REGISTRY\" | .repository = \"$REPOSITORY\"" > $MERIDIO_TARGET_HELM_PATH/values.yaml
     helm package $MERIDIO_TARGET_HELM_PATH --version $HELM_CHART_VERSION --destination $HELM_CHART_PATH
 }
 
 HELM_CHART_PATH="_output/helm/"
-NAMESPACE="{{.Release.Namespace}}"
 VERSION="${VERSION:-latest}"
 HELM_CHART_VERSION="$VERSION"
-REGISTRY="${REGISTRY:-'registry.nordix.org/cloud-native/meridio'}"
-NSM_REGISTRY_PATH="${NSM_REGISTRY_PATH:-'nordix/nsm'}"
+REGISTRY="${REGISTRY:-registry.nordix.org}"
+REPOSITORY="${REPOSITORY:-cloud-native/meridio}"
+NSM_REPOSITORY="${NSM_REPOSITORY:-cloud-native/nsm}"
 
 MERIDIO_TARGET_HELM_CURRENT_PATH="./examples/target/deployments/helm"
 
