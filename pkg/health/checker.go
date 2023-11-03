27c45438ee70b4e71f0ae912a53f3f6202811e13
Lugossy Zoltan <zoltan.lugossy@est.tech>
2023-11-03 15:40:02 +0100

    Custom gRPC health server improvements
    The same subservice can be part of multiple probe services.
    (Probe services are "named" kubernetes probe types referred
    to as "Startup", "Readiness" or "Liveness" in Meridio.)

--- pkg/health/checker.go
+++ pkg/health/checker.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Nordix Foundation
+Copyright (c) 2021-2023 Nordix Foundation
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -32,6 +32,11 @@ import (
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
+type serviceSetElem struct{}
+type serviceSet map[string]serviceSetElem
+
+var serviceMember serviceSetElem
+
 type HealthServerStatusModifier interface {
 	SetServingStatus(service string, servingStatus grpc_health_v1.HealthCheckResponse_ServingStatus)
 	Shutdown()
@@ -40,19 +45,22 @@ type HealthServerStatusModifier interface {
 type Checker struct {
 	grpc_health_v1.HealthServer
 	HealthServerStatusModifier
-	ctx               context.Context
-	listener          net.Listener
-	server            *grpc.Server
-	address           *url.URL
-	subServices       map[string][]string // subservices associated with a probe service
-	subServiceToProbe map[string]string   // map subservice to probe service
-	mu                sync.RWMutex
+	ctx                context.Context
+	listener           net.Listener
+	server             *grpc.Server
+	address            *url.URL
+	probeToSubServices map[string]serviceSet // set of subservices associated with a probe service
+	subServiceToProbes map[string]serviceSet // set of probe services associated with a subservice
+	mu                 sync.RWMutex
 }
 
 // RegisterServices -
 // Registers subservices for a probe service whose serving status will
 // be deterimined by the serving status of the subservices.
-// Note: currently a subservice can only belong to one probe service
+// Notes:
+//   - A subservice can be part of multiple probe services
+//   - No check for duplicate registration (e.g. services previously
+//     registered for the same probe service are not removed)
 func (c *Checker) RegisterServices(probeSvc string, services ...string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -62,40 +70,47 @@ func (c *Checker) RegisterServices(probeSvc string, services ...string) {
 		"Health server: Register subservices", "probeSvc", probeSvc,
 		"services", services)
 
-	components, ok := c.subServices[probeSvc]
-	if !ok {
-		// probe service not registered yet
-		components = []string{}
-		c.subServices[probeSvc] = components
+	subServices, ok := c.probeToSubServices[probeSvc]
+	if !ok { // new probe service
+		subServices = make(serviceSet)
+		c.probeToSubServices[probeSvc] = subServices
 	}
 	c.HealthServerStatusModifier.SetServingStatus(probeSvc, grpc_health_v1.HealthCheckResponse_NOT_SERVING)
 
-	// link subservices to the probe service
 	for _, s := range services {
-		c.subServiceToProbe[s] = probeSvc
+		subServices[s] = serviceMember // add subservices
+
+		// update map linking subservice to all related probe service
+		probeServices, ok := c.subServiceToProbes[s]
+		if !ok { // new subservice
+			probeServices = make(serviceSet)
+			c.subServiceToProbes[s] = probeServices
+		}
+		probeServices[probeSvc] = serviceMember
 		c.HealthServerStatusModifier.SetServingStatus(s, grpc_health_v1.HealthCheckResponse_NOT_SERVING)
 	}
-	c.subServices[probeSvc] = append(components, services...)
 }
 
 // Check -
 // Re-implements Check() function of grpc_health_v1.HealthServer interface
 // in order to log if a probe service is about to fail.
 func (c *Checker) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	var resp *grpc_health_v1.HealthCheckResponse
+	var err error
 
-	switch in.Service {
-	case Startup:
-		fallthrough
-	case Readiness:
-		fallthrough
-	case Liveness:
-		resp, err := c.HealthServer.Check(ctx, in)
-		if err == nil && resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
-			svcInfo := ""
-			// gather all related subservice' status to provide details on the failed probe
-			if len(c.subServices[in.Service]) > 0 {
-				svcInfo += `, subservices:[`
-				for _, s := range c.subServices[in.Service] {
+	logDetails := func(ctx context.Context) {
+		// gather all related subservice' status to provide details on the failed probe
+		var svcInfo string
+
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		if len(c.probeToSubServices[in.Service]) > 0 {
+			svcInfo += `, subservices:[`
+			for s := range c.probeToSubServices[in.Service] {
+				select {
+				case <-ctx.Done():
+					return
+				default:
 					svcInfo += fmt.Sprintf("%v:", s)
 					status := `N/A,`
 					if resp, err := c.HealthServer.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: s}); err == nil {
@@ -103,17 +118,34 @@ func (c *Checker) Check(ctx context.Context, in *grpc_health_v1.HealthCheckReque
 					}
 					svcInfo += fmt.Sprintf("%v", status)
 				}
-				svcInfo += `]`
 			}
-			logger := log.FromContextOrGlobal(c.ctx)
-			logger.Info(
-				"Probe service", "Service", in.Service,
-				"Status", resp.Status, "svcInfo", svcInfo)
+			svcInfo += `]`
 		}
-		return resp, err
-	default:
-		return c.HealthServer.Check(ctx, in)
+		logger := log.FromContextOrGlobal(c.ctx)
+		logger.Info(
+			"Probe service", "Service", in.Service,
+			"Status", resp.Status, "svcInfo", svcInfo)
 	}
+
+	switch in.Service {
+	case Startup:
+		fallthrough
+	case Readiness:
+		fallthrough
+	case Liveness:
+		resp, err = c.HealthServer.Check(ctx, in)
+		if err == nil && resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+			// gather all related subservice' status to provide details on the failed probe
+			logDetails(ctx)
+		}
+	default:
+		resp, err = c.HealthServer.Check(ctx, in)
+	}
+
+	if err != nil {
+		err = fmt.Errorf("%v check failed: %w", in.Service, err)
+	}
+	return resp, err
 }
 
 // Re-implements SetServingStatus function in order to evaluate probe service status
@@ -128,36 +160,38 @@ func (c *Checker) SetServingStatus(service string, servingStatus grpc_health_v1.
 	case Readiness:
 		fallthrough
 	case Liveness:
-		// in case of probe service only process serving status if no subservices
-		if components, ok := c.subServices[service]; ok && len(components) > 0 {
+		// for probe service (Startup/Readiness/Liveness) only process serving status if no subservices
+		if subServices, ok := c.probeToSubServices[service]; ok && len(subServices) > 0 {
 			break
 		}
 		c.HealthServerStatusModifier.SetServingStatus(service, servingStatus)
 	default:
 		{
 			c.HealthServerStatusModifier.SetServingStatus(service, servingStatus)
-			// determine serving status of the related probe service if any
-			if probeSvc, ok := c.subServiceToProbe[service]; ok {
-				// belongs to a probe service (i.e. it's a subservice)
-				if components, ok := c.subServices[probeSvc]; ok {
-					// probe has subservices; aggregate serving status of components to get the probe status
-					probeStatus := grpc_health_v1.HealthCheckResponse_NOT_SERVING
-					if grpc_health_v1.HealthCheckResponse_SERVING == servingStatus {
-						probeStatus = grpc_health_v1.HealthCheckResponse_SERVING
-						for _, component := range components {
-							// a subservice is not in SERVING status, the probe service must reflect it
-							resp, err := c.HealthServer.Check(c.ctx, &grpc_health_v1.HealthCheckRequest{Service: component})
-							if err != nil || (err == nil && resp.Status != grpc_health_v1.HealthCheckResponse_SERVING) {
-								probeStatus = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-								break
+			// determine serving status of all related probe service if any
+			if probeServices, ok := c.subServiceToProbes[service]; ok {
+				for probeSvc := range probeServices {
+					// belongs to a probe service (i.e. it's a subservice)
+					if subServices, ok := c.probeToSubServices[probeSvc]; ok { // probe was linked to service so it must have subservices
+						// aggregate serving status of subservices to get the probe status
+						// (if status of input service is not SERVING no need to check subservices)
+						probeStatus := grpc_health_v1.HealthCheckResponse_NOT_SERVING
+						if grpc_health_v1.HealthCheckResponse_SERVING == servingStatus {
+							probeStatus = grpc_health_v1.HealthCheckResponse_SERVING
+							for subService := range subServices {
+								// a subservice is not in SERVING status, the probe service must reflect it
+								resp, err := c.HealthServer.Check(c.ctx, &grpc_health_v1.HealthCheckRequest{Service: subService})
+								if err != nil || (err == nil && resp.Status != grpc_health_v1.HealthCheckResponse_SERVING) {
+									probeStatus = grpc_health_v1.HealthCheckResponse_NOT_SERVING
+									break
+								}
 							}
 						}
+						c.HealthServerStatusModifier.SetServingStatus(probeSvc, probeStatus)
 					}
-					c.HealthServerStatusModifier.SetServingStatus(probeSvc, probeStatus)
 				}
 			}
 		}
-
 	}
 }
 
@@ -173,7 +207,11 @@ func (c *Checker) Start() error {
 		<-c.ctx.Done()
 		c.server.Stop()
 	}()
-	return c.server.Serve(c.listener)
+	err := c.server.Serve(c.listener)
+	if err != nil {
+		return fmt.Errorf("grpc health server serve failed: %w", err)
+	}
+	return nil
 }
 
 // NewChecker -
@@ -211,20 +249,20 @@ func NewChecker(options ...Option) (*Checker, error) {
 		// remove possible lingering unix socket
 		err := os.Remove(target)
 		if err != nil && !errors.Is(err, os.ErrNotExist) {
-			return nil, fmt.Errorf("Failed to remove existing socket file: %v", err)
+			return nil, fmt.Errorf("failed to remove existing socket file: %w", err)
 		}
 		// create path if needed
 		basePath := path.Dir(target)
 		if _, err = os.Stat(basePath); os.IsNotExist(err) {
 			if err = os.MkdirAll(basePath, os.ModePerm); err != nil {
-				return nil, fmt.Errorf("Failed to create path for %v: %v", target, err)
+				return nil, fmt.Errorf("failed to create path for %v: %w", target, err)
 			}
 		}
 	}
 	// create listener for the URL
 	lis, err := net.Listen(network, target)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("grpc health server listen failed: %w", err)
 	}
 
 	s := grpc.NewServer()
@@ -237,8 +275,8 @@ func NewChecker(options ...Option) (*Checker, error) {
 		server:                     s,
 		address:                    address,
 		ctx:                        opts.ctx,
-		subServices:                make(map[string][]string),
-		subServiceToProbe:          make(map[string]string),
+		probeToSubServices:         make(map[string]serviceSet),
+		subServiceToProbes:         make(map[string]serviceSet),
 	}
 
 	grpc_health_v1.RegisterHealthServer(s, checker)
