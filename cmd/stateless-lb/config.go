3c4f9e41c3578ee8506ea5935720537bcdb94ea4
Lugossy Zoltan <zoltan.lugossy@est.tech>
2024-12-11 20:45:38 +0100

    lb; update neighbor cache on TAPA connection close
    Monitor TAPA->Proxy NSM connections and update neighbor
    cache upon connection close events by removing neighbor
    entries associated with TAPA side IP addresses.
    
    Currently, the connection monitor uses a wildcard filter,
    matching all connections.

--- cmd/stateless-lb/config.go
+++ cmd/stateless-lb/config.go
@@ -27,25 +27,27 @@ import (
 
 // Config for the proxy
 type Config struct {
-	Name                  string        `default:"load-balancer" desc:"Name of the pod"`
-	ServiceName           string        `default:"load-balancer" desc:"Name of providing service" split_words:"true"`
-	ConnectTo             url.URL       `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to NSM" split_words:"true"`
-	DialTimeout           time.Duration `default:"5s" desc:"timeout to dial NSMgr" split_words:"true"`
-	RequestTimeout        time.Duration `default:"15s" desc:"timeout to request NSE" split_words:"true"`
-	MaxTokenLifetime      time.Duration `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
-	NSPService            string        `default:"nsp-service:7778" desc:"IP (or domain) and port of the NSP Service" split_words:"true"`
-	ConduitName           string        `default:"load-balancer" desc:"Name of the conduit" split_words:"true"`
-	TrenchName            string        `default:"default" desc:"Trench the pod is running on" split_words:"true"`
-	LogLevel              string        `default:"DEBUG" desc:"Log level" split_words:"true"`
-	Nfqueue               string        `default:"0:3" desc:"netfilter queue(s) to be used by nfqlb" split_words:"true"`
-	NfqueueFanout         bool          `default:"false" desc:"enable fanout nfqueue option" split_words:"true"`
-	IdentifierOffsetStart int           `default:"5000" desc:"Each Stream will get a unique identifier range starting from that value" split_words:"true"`
-	GRPCKeepaliveTime     time.Duration `default:"30s" desc:"gRPC keepalive timeout" envconfig:"grpc_keepalive_time"`
-	GRPCProbeRPCTimeout   time.Duration `default:"1s" desc:"RPC timeout of internal gRPC health probe" envconfig:"grpc_probe_rpc_timeout"`
-	GRPCMaxBackoff        time.Duration `default:"5s" desc:"Upper bound on gRPC connection backoff delay" envconfig:"grpc_max_backoff"`
-	MetricsEnabled        bool          `default:"false" desc:"Enable the metrics collection" split_words:"true"`
-	MetricsPort           int           `default:"2223" desc:"Specify the port used to expose the metrics" split_words:"true"`
-	Socket                url.URL       `default:"unix:///var/lib/meridio/lb.sock" desc:"Server socket to host Stream Availability Service" split_words:"true"`
+	Name                       string        `default:"load-balancer" desc:"Name of the pod"`
+	ServiceName                string        `default:"load-balancer" desc:"Name of providing service" split_words:"true"`
+	ConnectTo                  url.URL       `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to NSM" split_words:"true"`
+	DialTimeout                time.Duration `default:"5s" desc:"timeout to dial NSMgr" split_words:"true"`
+	RequestTimeout             time.Duration `default:"15s" desc:"timeout to request NSE" split_words:"true"`
+	MaxTokenLifetime           time.Duration `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	NSPService                 string        `default:"nsp-service:7778" desc:"IP (or domain) and port of the NSP Service" split_words:"true"`
+	ConduitName                string        `default:"load-balancer" desc:"Name of the conduit" split_words:"true"`
+	TrenchName                 string        `default:"default" desc:"Trench the pod is running on" split_words:"true"`
+	LogLevel                   string        `default:"DEBUG" desc:"Log level" split_words:"true"`
+	Nfqueue                    string        `default:"0:3" desc:"netfilter queue(s) to be used by nfqlb" split_words:"true"`
+	NfqueueFanout              bool          `default:"false" desc:"enable fanout nfqueue option" split_words:"true"`
+	IdentifierOffsetStart      int           `default:"5000" desc:"Each Stream will get a unique identifier range starting from that value" split_words:"true"`
+	GRPCKeepaliveTime          time.Duration `default:"30s" desc:"gRPC keepalive timeout" envconfig:"grpc_keepalive_time"`
+	GRPCProbeRPCTimeout        time.Duration `default:"1s" desc:"RPC timeout of internal gRPC health probe" envconfig:"grpc_probe_rpc_timeout"`
+	GRPCMaxBackoff             time.Duration `default:"5s" desc:"Upper bound on gRPC connection backoff delay" envconfig:"grpc_max_backoff"`
+	MetricsEnabled             bool          `default:"false" desc:"Enable the metrics collection" split_words:"true"`
+	MetricsPort                int           `default:"2223" desc:"Specify the port used to expose the metrics" split_words:"true"`
+	Socket                     url.URL       `default:"unix:///var/lib/meridio/lb.sock" desc:"Server socket to host Stream Availability Service" split_words:"true"`
+	Namespace                  string        `default:"default" desc:"Namespace the pod is running on" split_words:"true"`
+	TargetDisconnectMonitoring bool          `default:"true" desc:"Enable listening to Target disconnect events to clean-up linux neighbor cache" split_words:"true"`
 }
 
 // IsValid checks if the configuration is valid
