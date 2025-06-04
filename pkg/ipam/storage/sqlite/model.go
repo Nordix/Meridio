f62efe9a04cb5d8d66317d6c7f731a553acdf848
Lugossy Zoltan <zoltan.lugossy@est.tech>
2025-06-04 15:25:59 +0200

    ipam; Add CIDR uniqueness constraint to sqlite model
    Sqlite model is updated to enforce uniqueness based on parent_id
    and cidr fields via database constraints, directly reflecting the
    IP allocation logic's expectations regarding CIDR uniqueness within
    the same parent.
    
    The model change greatly complements the allocation logic's collision
    resolution mechanism, and helps in case of concurrent attempts to
    avoid constant add/delete churn without stable allocation.

--- pkg/ipam/storage/sqlite/model.go
+++ pkg/ipam/storage/sqlite/model.go
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2021 Nordix Foundation
-Copyright (c) 2024 OpenInfra Foundation Europe
+Copyright (c) 2024-2025 OpenInfra Foundation Europe
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,21 +18,40 @@ limitations under the License.
 package sqlite
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
+	"github.com/mattn/go-sqlite3"
 	"github.com/nordix/meridio/pkg/ipam/prefix"
 	"github.com/nordix/meridio/pkg/ipam/types"
 )
 
 type Prefix struct {
 	Id        string `gorm:"primaryKey"`
-	Name      string `gorm:"index"` // supposedly indexing could improve query performance
-	Cidr      string
-	ParentID  string `gorm:"index"`
+	Name      string `gorm:"index"`                          // supposedly indexing could improve query performance
+	Cidr      string `gorm:"uniqueIndex:idx_parent_id_cidr"` // composite uniqueIndex should be helpful in resolving race conditions in concurrent allocation attempts considering the hierarchical allocation logic of prefix.Allocate
+	ParentID  string `gorm:"index;uniqueIndex:idx_parent_id_cidr"`
 	Parent    *Prefix
 	UpdatedAt time.Time `gorm:"index"`               // supposedly indexing could improve query performance
 	Expirable *bool     `gorm:"index;default:false"` // indicates whether prefix can expire and thus be subject to garbage collection
+}
+
+// isCIDRUniquenessViolation checks if error code reports unique index violation for CIDR
+// Note: Error string is not matched as it would make the code brittle, however the check
+// exploits the fact that the model contains only one (composite) unique index.
+func isCIDRUniquenessViolation(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	var sqliteErr sqlite3.Error
+	if errors.As(err, &sqliteErr) {
+		if sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return true
+		}
+	}
+	return false
 }
 
 func modelToPrefix(p *Prefix, parent types.Prefix) types.Prefix {
